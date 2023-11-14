4c91c125b0e8ec1c9f2c7f1313099fbf502f16b7
Kusalava0 <dkb03122001@gmail.com>
2023-11-14 15:27:54 +0530

    adding remaining pages without images

--- src/components/Line/Line.jsx
+++ src/components/Line/Line.jsx
@@ -1,4 +1,3 @@
-// import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 import './Line.css'
@@ -6,15 +5,63 @@ import './Line.css'
 function Line() {
   return (
     <>
-    <div>
-        <Navbar/>
-    </div>
-    <div>
-        
-    </div>
-    <div>
-        <Footer/>
-    </div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <div className='line-container'>
+          <h1>Line of Activity</h1>
+          <h2>Discovery</h2>
+          <p>
+            Enzyme discovery marks the start of the journey for Novel Enzyme Development. Our low-risk concept starts with fully understanding your needs and processes, before identifying the appropriate gene sequence to perform an initial proof-of-concept fermentation of your new enzyme. As an Enzyme Development and Manufacturing Company, our overall aim is to partner with you towards a continuous supply of your desired Novel Enzyme at the right price.
+          </p>
+          <h2>Bioinformatics, Cloning & Metagenomics</h2>
+          <p>
+            Find the right Enzyme for your process by using Biocatalysts’ in-house Enzyme Discovery Technology
+          </p>
+          <h2>Enzyme samples</h2>
+          <p>
+            Biocatalysts can turn a concept into a reality by delivering a sample of active, soluble enzyme along with a technical summary and specification.
+          </p>
+          <h2>Fermentation development and optimisation</h2>
+          <p>
+            Our focus in the development phase is on producing the maximum possible enzyme titre, purity, and yield. Now that the enzyme has been shown to perform as expected, the final stage involves making the product fit your commercial requirements.
+          </p>
+          <h2>Scale-up to Manufacture</h2>
+          <p>
+            The enzyme landscape is complex. We support our customers in ensuring their exclusive enzyme is compliant for their appropriate commercial uses.
+          </p>
+          <h2>Small Molecules</h2>
+          <div className="flex-container">
+          <div className='samll-mole-container'>
+
+            <h3>Biologics</h3>
+            <p>Integrated Drug Discovery</p>
+          </div>
+          <div className='samll-mole-container'>
+
+            <h3>Drug Metabolites</h3>
+            <p>Peptides, Olegonucleotides, Antibody Drug Conjugates, PRPTAC, CAR-T, PDNA& mRNA, Rare Orphan Diseases</p>
+          </div>
+            <div className='samll-mole-container'>
+
+            <h3>Industries and Emerging Biopharma</h3>
+            <p>Animal Health, Pharma Biotech, Agrochemical, Nutrition, Specialty Materials.</p>
+            </div>
+          </div>
+          <h2>Discovery</h2>
+          <p>
+            Chemical, Biological, Competition & Data Science
+          </p>
+          <h2>Development</h2>
+          <p>
+            Chemical Development, Formulation Development, Clinical Development
+          </p>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </>
   )
 }
--- src/components/Pulmonary/Pulmonary.jsx
+++ src/components/Pulmonary/Pulmonary.jsx
@@ -9,8 +9,26 @@ function Pulmonary() {
     <div>
         <Navbar/>
     </div>
-    <div>
-        
+    <div className='line-container'>
+        <h2>Pulmonary Drug delivery</h2>
+        <ul className='ui-container'>
+          <li>
+          Diseases: ARDS, COVID-19, Asthma, COPD, Pulmonary Fibrosis, TB and Leprosy
+          </li>
+          <li>
+          Localized delivery into lungs though inhalation would directly target the disease area, while avoiding the systemic exposure
+          </li>
+          <li>
+          Can control the spread of infectious diseases, while protecting from severe lung conditions
+          </li>
+          <li>Attempts will be made to develop innovative inhalational products</li>
+          <li>
+          Inhalation dosage forms or inhalation preparations refer to any drug product or drug solution taken via the inhalation route of administration (the nasal or respiratory route). Inhalation dosage forms are intended to deliver drug substances to the lungs. These inhaled dosage forms for drug delivery to the lungs affect pulmonary function or treat allergic symptoms in the respiratory tract. Inhalation preparations are generally formulated as solutions, suspensions, and powders.
+          </li>
+          <li>
+          These solution, suspension, and powder formulations are delivered to the respiratory tract via drug delivery devices like aerosols, dry powder inhalers (DPI), metered dose inhalers (MDI), and nebulizers, amongst other devices. Inhalation preparations are most commonly used to treat respiratory ailments such as asthma, chronic bronchitis, emphysema, and bronchiectasis.
+          </li>
+        </ul>
     </div>
     <div>
         <Footer/>
