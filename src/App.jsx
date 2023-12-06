240ddb5c15f3ce7529c979fafca0aba18bfcdac1
Kusalava0 <dkb03122001@gmail.com>
2023-12-06 14:02:37 +0530

    using hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route  } from 'react-router-dom';
 import './App.css'
 import Home from './components/Home/Home'
 import AboutUs from './components/AboutUs/AboutUs'
